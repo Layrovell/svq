9883d77aac10a2503246607659b68af745d84835
Marina Prodan <marinap@sweetsoft.com.ua>
2021-04-13 18:55:31 +0300

    app init

---
+++ src/routes/quiz/[run].svelte
@@ -0,0 +1,14 @@
+<script context="module">
+    import { writable } from "svelte/store";
+    import { quiz } from "main/quiz";
+
+    const runStore = writable(null);
+
+    export function preload(page, session) {
+        runStore.set(page.params.run);
+        quiz.set(page.params.run);
+    }
+
+</script>
+
+our run = {$runStore}
---
+++ src/routes/quiz/_layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { quiz } from "main/quiz";
+</script>
+
+<style>
+    div { 
+        border-top: 1px solid red;
+    }
+</style>
+
+<slot/>
+
+<div>current quiz {$quiz}</div>
